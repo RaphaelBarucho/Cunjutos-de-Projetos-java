56468b70a56a9ceb51b1f4161d2bbcdb06c65ef6
Raphael Barucho <134150030+RaphaelBarucho@users.noreply.github.com>
2024-05-02 23:42:43 -0300

    Add files via upload

---
+++ vetor_de_nove_numero.java
@@ -0,0 +1,30 @@
+package projeto;
+import java.util.Scanner;
+public class vetor_de_nove_numero {
+
+	public static void main(String[] args) {
+		int[] num = new int[9];
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < 9; i++) {
+            System.out.print("Digite o número " + (i + 1) + ": ");
+            num[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < 9; i++) {
+            int cont = 0;
+            for (int j = 1; j <= num[i]; j++) {
+                if (num[i] % j == 0) {
+                    cont++;
+                }
+            }
+            if (cont <= 2) {
+                System.out.println("Número " + num[i] + " tem " + cont + " divisores.");
+                System.out.println("Posição: " + (i + 1));
+            }
+            sc.close();
+        }
+
+	}
+
+}
